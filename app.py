7269c951c7236d105d04688342bacfe1dc1e6d17
divya tv <divya.tv@gmail.com>
2019-08-24 20:06:07 -0400

    adding homework files

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import pymongo
+import json
+import scrape_mars
+import pandas as pd
+
+# Create an instance of Flask
+app = Flask(__name__, static_url_path='/static')
+#app = Flask(__name__, template_folder='/templates', static_folder='/static')
+
+# Use PyMongo to establish Mongo connection
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+db = client.mars_page
+#collection = db.mars_data
+### delete the data if there are any to start clean
+#db.collection.delete_many({})
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    #destination_data = db.collection.find_one()
+    destination_data = db.collection.find_one({})
+    if (destination_data!=None):
+        ### Get the table from mongodb and fix it
+        dict1=destination_data['mars_dataframe']
+        import ast
+        x=ast.literal_eval(dict1)
+        print(len(x))
+        newdf=pd.DataFrame({})
+        for i in x:
+            newdf=newdf.append(pd.DataFrame([i]))
+        import io
+        newdf=newdf.rename(columns={"0": "Dimensions", "1": " "})
+        newdf=newdf.set_index(['Dimensions'])
+        str_io=io.StringIO()
+        newdf.to_html(buf=str_io, classes='table table-stripped')
+        html_str=str_io.getvalue()
+    
+        # Return template and data
+        return render_template("template.html", mars=destination_data, tables=[html_str], titles=newdf.columns.values)
+    else:
+        return render_template("template.html", mars=None, tables=[None], titles=None)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    ### Convert the dataframe into a dictionary to store in the database.
+    mars_dataframe=scrape_mars.mars_facts()
+    records=mars_dataframe.to_json(None, orient='records')
+    mars_news=scrape_mars.mars_news_title()
+
+    mars_data_dict={
+            "fullresolimage": scrape_mars.background_image(),
+            "mars_news_title":mars_news[0],
+            "mars_news_content": mars_news[1],
+            "mars_weather":scrape_mars.mars_weather(),
+            "mars_dataframe":records,
+            "mars_hemisphere_images":scrape_mars.mars_hemisphere_images()
+        }
+
+    db.collection.insert_one(mars_data_dict)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,163 @@
+from bs4 import BeautifulSoup
+import requests
+from splinter import Browser
+
+
+def mars_news_title():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    url = 'https://mars.nasa.gov/#news_and_events'
+
+    baseurl='https://mars.nasa.gov'
+        
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # with open("mars_news.html", "w", encoding='utf-8') as file:
+    #     file.write(str(soup))
+        
+    # with open("mars_news.html", "r", encoding='utf-8') as file:
+    #     soup=file.read()
+        
+    soup = BeautifulSoup(html, 'html.parser')
+        
+    latest_news_mars = soup.find('ul', class_='item_list list_view')
+    # print(latest_news_mars)
+    #news_title=latest_news_mars.find('div', {'class':'list_image'}).find('img')['alt']
+    
+    new_title_changed_page=latest_news_mars.find('h3', {'class':'title'}).find('a')['href']
+    news_p=latest_news_mars.find('h3', {'class':'title'}).find('a').get_text()
+       
+    next_url=baseurl + new_title_changed_page
+    print(next_url)
+    ################### taking the content #######################
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    
+    browser.visit(next_url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    mars_news_content=soup.find('div', {'class':'wysiwyg_content'}).find('p').get_text()
+    #.get_text()
+
+    return_list=[mars_news_content,news_p]
+    return(return_list)
+
+
+
+def background_image():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+            
+    mars_featured_picture=soup.find('article')        
+
+    mars_featured_picture_jpg=mars_featured_picture.get('style')
+    url=mars_featured_picture_jpg.split("(")
+    url1=url[1].split(");")
+    
+    import re
+    image_url=url1[0].replace("'", "")
+    #print(image_url)
+    ### JPL Mars Space Images - Featured Image
+    ## Build the url for mars image
+    featured_image_url = 'https://www.jpl.nasa.gov' + image_url
+    #print("featured_image_url for mars=",featured_image_url)
+
+    return (featured_image_url)
+
+def mars_weather():
+        #### Mars weather
+    #https://twitter.com/marswxreport?lang=en
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    url = 'https://twitter.com/marswxreport?lang=en'
+
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    import requests
+    from splinter.exceptions import ElementDoesNotExist
+       
+    mars_weather=soup.find_all('p', class_='TweetTextSize')
+    mars_weather=mars_weather[0].get_text()
+    import re
+    mars_weather=re.sub("InSight", " ",mars_weather)
+    mars_weather=re.sub("hPapic.twitter.com/MhPPOHJg3m", " ",mars_weather)
+    return(mars_weather)
+
+
+def mars_facts():
+        ### Mars Facts - fetch mars facts as a table.
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    url = 'https://space-facts.com/mars/'
+
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    mars_facts=soup.find(id='text-2')
+  # print(mars_facts)
+    import pandas as pd
+    import numpy 
+    # type(mars_facts)
+    mars_dataframe = pd.read_html(mars_facts.prettify())[0]
+    #mars_dataframe=mars_dataframe.drop(columns=['Earth'])
+    return(mars_dataframe.head(15))
+
+def mars_hemisphere_images():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    browser.visit(url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    #with open("mars_cerberus_hemisphere.html", "w", encoding='utf-8') as file:
+        #file.write(str(soup))
+    mars_ch=soup.find_all('a', class_="itemLink")    
+    base_url_image='https://astrogeology.usgs.gov'
+  
+  #### I could use a for loop but had to deliver the cup cake by today so all the drama below:
+    cerberus=mars_ch[0]
+    cerberus_url=cerberus.find('img')['src']
+    cerberus_title=cerberus.find('img')['alt']
+    print(cerberus_title)
+    print(base_url_image+cerberus_url)
+
+    schiaparelli=mars_ch[2]
+    schiaparelli_url=schiaparelli.find('img')['src']
+    schiaparelli_title=schiaparelli.find('img')['alt']
+    print(schiaparelli_title)
+    print(base_url_image+schiaparelli_url)
+
+    syrtis_major=mars_ch[4]
+    syrtis_major_url=syrtis_major.find('img')['src']
+    syrtis_major_title=syrtis_major.find('img')['alt']
+    print(syrtis_major_title)
+    print(base_url_image+syrtis_major_url)
+
+    valles_marineris=mars_ch[6]
+    valles_marineris_url=valles_marineris.find('img')['src']
+    valles_marineris_title=valles_marineris.find('img')['alt']
+    print(valles_marineris_title)
+    print(base_url_image+valles_marineris_url)
+   
+    summary_dict={cerberus_title:base_url_image+cerberus_url, 
+              schiaparelli_title: base_url_image+schiaparelli_url,
+              syrtis_major_title:base_url_image+syrtis_major_url,
+              valles_marineris_title:base_url_image+valles_marineris_title              
+             }
+
+    return(summary_dict)
+
+
+
+  
